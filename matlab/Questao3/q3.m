2af790738a4c22c478c885e1421f7306c9c3ba8e
idoria75 <ivanp_doria@hotmail.com>
2018-09-29 20:14:42 -0300

    Move files to folders

--- matlab/Questao3/q3.m
+++ matlab/Questao3/q3.m
@@ -3,7 +3,7 @@ clear;
 clc;
 close all;
 
-%% Questao 3.1: Modelar sistema em espaco de estados
+%% Questao 3.1, 3.3: Modelar sistema em espaco de estados
 
 % Definicoes (igual da q2, ver como obter este modelo!!)
 m = 0.5;
@@ -35,10 +35,15 @@ uf = [u1;
       u2;
       u3;
       u4];
+  
+A = subs(jacobian(f,x),[x1 x2 x3 x4],[0 0 0 0])
+B = subs(uf,[x3 x4],[0 0])
+C = [1 0 0 0]
+D = 0
 
-A = subs(jacobian(f,x),[x1 x2 x3 x4],[0 0 0 0]);
-B = subs(uf,[x3 x4],[0 0]);
-C0 = 0;
+disp('Controlabilidade e Observabilidade ja verificadas na Q2');
+
+%% Questao 3.4: 
 
 x0 = [0 0 pi/180*25 0];
 x0obs = [0; 0; pi/180*10; 0];
