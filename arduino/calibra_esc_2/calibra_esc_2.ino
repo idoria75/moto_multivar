784230cdcfe70906e2af3cd0dcd811b005d22327
Ivan Posca Doria <ivanpdoria@gmail.com>
2018-12-15 12:24:39 -0200

    Updates txt and add pdf file

--- arduino/calibra_esc_2/calibra_esc_2.ino
+++ arduino/calibra_esc_2/calibra_esc_2.ino
@@ -1,36 +1,45 @@
+// Program to calibrate ESCs following the manual:
+// https://www.firediy.fr/files/drone/HW-01-V4.pdf
+
 #include <Servo.h>
 
 Servo mot;
 int pin_mot = 9;
-int data;
-int pwm_value = 1001;
+int pwm_min = 1000;
+int pwm_max = 2000;
+char data, old_data;
+unsigned long count = 0;
 
 void setup() {
   Serial.begin(115200);
-  mot.attach(pin_mot,1000,2000);
-  mot.writeMicroseconds(1000);
-  
-  Serial.println("SetUp Ready");
+  mot.attach(pin_mot,pwm_min,pwm_max);
+  mot.writeMicroseconds(pwm_min);
+  Serial.println("ESC CALIBRATOR");
+  Serial.println("Instructions: ");
+  Serial.println("Please, enter i to set pwm to max and o to set pwm to min.");
+  Serial.println("Follow the .txt file with the instructions");
+  Serial.println("");
 }
 
 void loop() {
   if(Serial.available()){
-    data = Serial.parseInt();
-    if(data != pwm_value){
-      if((data >= 1000) and (data <= 2000)){
-        pwm_value = data;
-        mot.writeMicroseconds(pwm_value);
-        Serial.print("PWM Write: "); Serial.println(pwm_value);
+    data = Serial.read();
+    if(data != old_data){
+      count++;
+      old_data = data;
+      switch(data){
+        case 'o':
+          mot.writeMicroseconds(pwm_min);
+          Serial.print(count); Serial.println(": PWM set to MIN");
+          break;
+        case 'i':
+          mot.writeMicroseconds(pwm_max);
+          Serial.print(count); Serial.println(": PWM set to MAX");
+          break;
+        default:
+          Serial.print(count); Serial.println(": Please enter i(Max) or o(Min)");
+          break;
       }
-      else{
-        Serial.println("Input is not in range 1000-2000");
-        Serial.print("Input: ");
-        Serial.println(data);
-      }
-    }
-    else{
-      Serial.println("Input not different from previous one");
     }
   }
-  delay(200);
 }
