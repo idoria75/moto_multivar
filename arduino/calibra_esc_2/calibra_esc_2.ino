36a101d48d683df43205c3f38c6de701c10b9e88
Ivan Posca Doria <ivanpdoria@gmail.com>
2018-12-15 11:43:11 -0200

    Hotfix

--- arduino/calibra_esc_2/calibra_esc_2.ino
+++ arduino/calibra_esc_2/calibra_esc_2.ino
@@ -1,42 +1,36 @@
 #include <Servo.h>
 
 Servo mot;
+int pin_mot = 9;
 int data;
 int pwm_value = 1001;
-int pwm_value_old = pwm_value;
-
-int flag = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  mot.attach(9,1000,2000);
+  Serial.begin(115200);
+  mot.attach(pin_mot,1000,2000);
   mot.writeMicroseconds(1000);
   
   Serial.println("SetUp Ready");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   if(Serial.available()){
-    pwm_value=Serial.parseInt();
-    //pwm_value = atoi(data);
-    if(pwm_value_old != pwm_value){
-      pwm_value_old = pwm_value;
-      Serial.print("Wrinting value: ");
-      Serial.println(pwm_value);
-      flag = 1;
-      
+    data = Serial.parseInt();
+    if(data != pwm_value){
+      if((data >= 1000) and (data <= 2000)){
+        pwm_value = data;
+        mot.writeMicroseconds(pwm_value);
+        Serial.print("PWM Write: "); Serial.println(pwm_value);
+      }
+      else{
+        Serial.println("Input is not in range 1000-2000");
+        Serial.print("Input: ");
+        Serial.println(data);
+      }
     }
     else{
-      Serial.print("Value: ");
-      Serial.println(data);
-      delay(100);
+      Serial.println("Input not different from previous one");
     }
   }
-  if((pwm_value >= 1000) and (pwm_value <= 2000) and (flag == 1)){
-    mot.writeMicroseconds(pwm_value);
-    Serial.print("PWM Write: "); Serial.println(pwm_value);
-    flag = 0;
-  }
+  delay(200);
 }
